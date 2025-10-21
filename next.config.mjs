5c60d4a55fe3f83a6926ddcbf61edda4a4eaf91a
Sujit Laware <pointolearnersofficial@gmail.com>
2025-10-21 14:13:12 +0530

    V

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,16 @@
 ﻿const isDev = process.env.NODE_ENV === 'development';
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  output: isDev ? undefined : 'export',
-  distDir: isDev ? '.next' : 'out',
+/** @type {import("next").NextConfig} */
+const config = {
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  output: process.env.CF_PAGES ? undefined : "export",
+  distDir: process.env.CF_PAGES ? ".vercel/output/static" : "out",
   trailingSlash: true,
   images: {
     unoptimized: true,
-    domains: ['localhost'],
-  },
+    domains: ["localhost"],
+  }
 };
 
-export default nextConfig;
+export default config;
